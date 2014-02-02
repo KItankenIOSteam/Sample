e621f679711bfd6c3538e60ec9862f48fd3fa14f
jineq <e115765@iu.u-ryukyu.ac.jp>
2014-02-02 17:58:08 +0900

    First commit

---
+++ test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  test
+//
+//  Created by vilayouth vongsomxai on 2014/02/02.
+//  Copyright (c) 2014年 Apportable. All rights reserved.
+//
+
+#import "CCButton.h"
+
+@interface test : CCNode
+
+@end
---
+++ test.m
@@ -0,0 +1,15 @@
+//
+//  test.m
+//  test
+//
+//  Created by vilayouth vongsomxai on 2014/02/02.
+//  Copyright (c) 2014年 Apportable. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+-(void)push:(id)sender {
+    NSLog(@"pushed yeah");
+}
+@end
